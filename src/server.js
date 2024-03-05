9deefdb3dab2d618ae84a7be1fbe445928e7ec50
antonio <antoniorc7@gmail.com>
2024-03-05 13:08:03 +0100

    feat: router added

--- src/server.js
+++ src/server.js
@@ -2,12 +2,25 @@
 import express from "express"
 import 'dotenv/config'
 import { dbConnection } from "./database/db.js";
+import router from "./routes/auth.routes.js";
 
 const app = express()
 
 app.use(express.json())
 
 const PORT = process.env.PORT || 4001;
+
+//API ROUTES
+
+app.get('/api/healthy', (req, res) => {
+    res.status(200).json({
+        success: true,
+        message: "Server is healthy"
+    })
+})
+
+app.use('/api', router)
+
 
 dbConnection()
 .then(() => {
@@ -19,4 +32,3 @@ dbConnection()
 .catch(error => {
     console.log(error)
 })
-
